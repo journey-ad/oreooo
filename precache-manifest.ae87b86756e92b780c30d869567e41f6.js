da807e3e141c71aa22eeb8555d0bf7a4d156c54c
journey-ad <journey.adc@gmail.com>
2019-04-15 00:47:14 +0800

    init gh-pages

---
+++ precache-manifest.ae87b86756e92b780c30d869567e41f6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bc0acbf8bf67e79336a",
+    "url": "./css/app.b28dfba7.css"
+  },
+  {
+    "revision": "9bc0acbf8bf67e79336a",
+    "url": "./js/app.dab099e9.js"
+  },
+  {
+    "revision": "3d342a8cdfe6b4e660d0",
+    "url": "./js/chunk-vendors.dbacda76.js"
+  },
+  {
+    "revision": "f55d75eb5275de80d836d268cb71ac29",
+    "url": "./img/oreo.f55d75eb.png"
+  },
+  {
+    "revision": "86adc167d797d4659fe119634d127338",
+    "url": "./img/O.86adc167.png"
+  },
+  {
+    "revision": "756f1d7e1baaec13142c854251b5b3c1",
+    "url": "./fonts/Seto.756f1d7e.eot"
+  },
+  {
+    "revision": "1f51690c34e46733fce1ae496f94a863",
+    "url": "./fonts/Seto.1f51690c.woff"
+  },
+  {
+    "revision": "1396d8cdbd360f25314901472526c55d",
+    "url": "./fonts/Seto.1396d8cd.ttf"
+  },
+  {
+    "revision": "d0ba91dcd81ffed933e9f4e5f657bb1d",
+    "url": "./img/Seto.d0ba91dc.svg"
+  },
+  {
+    "revision": "0ecc63ff81b3cef910615fb1ace7cc11",
+    "url": "./img/random.0ecc63ff.svg"
+  },
+  {
+    "revision": "8274afb073f69f59829439ab4b9b3693",
+    "url": "./img/tip.8274afb0.svg"
+  },
+  {
+    "revision": "264cd9d8c69303a36dba3cfe7a4706ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "./robots.txt"
+  }
+];
